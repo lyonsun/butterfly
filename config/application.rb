c1bae0195a495ddf808fe31125aa318fe80c6275
Lyon Sun <sunly917@gmail.com>
2016-04-11 13:33:57 +0300

    add chinese localization

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module Butterfly
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :cn
 
     config.generators do |g|
       g.orm :mongoid
